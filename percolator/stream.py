fd6fcd15fe7cb55182c3e88623f565c7c029c857
Vasili Vasilyeu <vasili.v@tut.by>
2014-08-12 11:48:43 +0300

    Added thread for stream reading; Fixed stream cleanup

--- percolator/stream.py
+++ percolator/stream.py
@@ -1,31 +1,51 @@
 import os
+import threading
 
 class Stream(object):
     def __init__(self, event):
         self.__event = event
 
+        self.__thread = None
         self.__out_descriptor = None
         self.__in_descriptor = None
 
     def __clean(self):
-        if self.__out_descriptor is not None:
-            try:
-                os.close(self.__out_descriptor)
-            finally:
-                self.__out_descriptor = None
+        self.__thread = None
+        try:
+            os.close(self.__out_descriptor)
+        except:
+            pass
+        self.__out_descriptor = None
 
-        if self.__in_descriptor is not None:
-            try:
-                os.close(self.__in_descriptor)
-            finally:
-                self.__in_descriptor = None
+        try:
+            os.close(self.__in_descriptor)
+        except:
+            pass
+        self.__in_descriptor = None
+
+    def __source(self):
+        while not self.__event.is_set():
+            self.__event.wait(.1)
+
+    def __start(self):
+        self.__thread = threading.Thread(None, self.__source)
+        self.__thread.start()
 
     def get_descriptor(self):
         if self.__out_descriptor is None:
             self.__clean()
-            self.__out_descriptor, self.__in_descriptor = os.openpty()
+            try:
+                self.__out_descriptor, self.__in_descriptor = os.openpty()
+                self.__start()
+            except:
+                self.__clean()
+                raise
 
         return self.__out_descriptor
 
     def stop(self):
+        try:
+            self.__thread.join()
+        except:
+            pass
         self.__clean()
--- test/test_stream.py
+++ test/test_stream.py
@@ -1,24 +1,50 @@
 import unittest
+import threading
 
 from percolator.stream import Stream
 
 class TestStream(unittest.TestCase):
+    def setUp(self):
+        self.event = threading.Event()
+        self.event.clear()
+
+    def tearDown(self):
+        if hasattr(self, 'event'):
+            self.event.set()
+
+        if hasattr(self, 'stream'):
+            self.stream.stop()
+            del self.stream
+
+        if hasattr(self, 'event'):
+            del self.event
+
     def test_creation(self):
         Stream(None)
 
     def test_get_descriptor(self):
-        stream = Stream(None)
-        self.assertTrue(isinstance(stream.get_descriptor(), (int, long)))
+        self.stream = Stream(self.event)
+        self.assertTrue(isinstance(self.stream.get_descriptor(), (int, long)))
+
+    def test_get_descriptor_exception(self):
+        self.stream = Stream(self.event)
+
+        def exception_start():
+            raise Exception()
+
+        self.stream._Stream__start = exception_start
+        self.assertRaises(Exception, self.stream.get_descriptor)
 
     def test_get_descriptor_twice(self):
-        stream = Stream(None)
-        descriptor = stream.get_descriptor()
-        self.assertEqual(stream.get_descriptor(), descriptor)
+        self.stream = Stream(self.event)
+        descriptor = self.stream.get_descriptor()
+        self.assertEqual(self.stream.get_descriptor(), descriptor)
 
     def test_stop(self):
-        stream = Stream(None)
-        stream.get_descriptor()
-        stream.stop()
+        self.stream = Stream(self.event)
+        self.stream.get_descriptor()
+        self.event.set()
+        self.stream.stop()
 
 if __name__ == '__main__':
     unittest.main()
