78ad58c1631283dbb5467905dd723fff6017a9f3
Vasili Vasilyeu <vasili.v@tut.by>
2014-08-12 09:52:07 +0300

    Added dummy library

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ percolator
 ==========
 
 Run script and parse its output
+
+Build
+-----
+
+python setup.py build
+
+Install
+-------
+
+python setup.py install
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Percolator',
+      version='0.0.1',
+      description='Run script and parse its output',
+      author='Vasili Vasilyeu',
+      author_email='vasili.v@tut.by',
+      url='https://github.com/vasili-v/percolator',
+      packages=['percolator'])
+
